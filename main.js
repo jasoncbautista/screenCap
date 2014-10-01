bb1d92d51f7bed59b23352eb7e25d8b8712beca1
Jason <bjason@gmail.com>
2014-09-30 23:15:46 -0700

    Span, set timeout

--- main.js
+++ main.js
@@ -1,7 +1,2 @@
 console.log("main");
-setTimeout(function(){
-    html2canvas(document.body).then(function(canvas) {
-        debugger;
-        document.body.appendChild(canvas);
-    });
-}, 2500);
+
